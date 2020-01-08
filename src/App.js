64df4d39d6db937395a9c9a4fd9ffab6a509fb49
iamnehaandroid <nssharma4481@gmail.com>
2020-01-08 15:47:21 +0530

    extends class and functional components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import React, {Component} from 'react';
+import React , { Component } from 'react';
+
 import logo from './logo.svg';
 import './App.css';
-import Greet from 'D:/helloworld/src/components/Greet';
-
+import { Greet } from './components/Greet';
+import Welcome from './components/Welcome';
 class App extends Component {
   render(){
   return (
     <div className="App">
    <Greet   />
-      
+   <Welcome  />   
     </div>
   );
 }
