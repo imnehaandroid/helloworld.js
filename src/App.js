720bd0fa6ebf1051382c7bcd3c60002e4b5dcf1a
iamnehaandroid <nssharma4481@gmail.com>
2020-01-09 11:39:34 +0530

    implement reactJS properties

--- src/App.js
+++ src/App.js
@@ -2,16 +2,26 @@ import React , { Component } from 'react';
 
 import logo from './logo.svg';
 import './App.css';
-import { Greet } from './components/Greet';
+import  { Greet }  from './components/Greet';
 import Welcome from './components/Welcome';
 import Hello from './components/Hello'
 class App extends Component {
   render(){
   return (
     <div className="App">
-   {/*<Greet   />*/}
-    {/*    <Welcome  /> */} 
-     <Hello /> 
+    <Greet name="Bruce" heroName ="Batman">
+     <p>This is chlidren props </p>
+    </Greet>
+    <Greet name="Clark" heroName ="Superman">
+     <button>Action</button>
+    </Greet>  
+    <Greet name="Diana" heroName = "Wonder Woman"/>
+
+    <Welcome name="Bruce" heroName ="Batman"  /> 
+    <Welcome name="Clark" heroName ="Superman" /> 
+    <Welcome name="Diana" heroName = "Wonder Woman"  /> 
+    
+    {/*<Hello /> */} 
     </div>
   );
 }
--- src/components/Greet.js
+++ src/components/Greet.js
@@ -4,5 +4,12 @@ import React from 'react';
 //function Greet(){
     //return <h1> Hello Neha</h1>
 //}
-  export const Greet = () => <h1> Hello Neha </h1>
+  export const Greet = props => {
+    console.log(props)
+  return <div>
+     <h1> Hello  { props.name }  a.k.a { props.heroName}</h1>
+                 {props.children}
+       </div>
+   }
+   
 //export default Greet;
