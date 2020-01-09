5b766fd1980e6819a77488c5bf295b55d02338c2
iamnehaandroid <nssharma4481@gmail.com>
2020-01-09 14:59:30 +0530

    using counter and implement the setState method

--- src/App.js
+++ src/App.js
@@ -6,12 +6,14 @@ import  { Greet }  from './components/Greet';
 import Welcome from './components/Welcome';
 import Hello from './components/Hello'
 import Message from './components/Message'
+import Counter from './components/Counter'
 class App extends Component {
   render(){
   return (
     <div className="App">
-    <Message />  
+     <Counter />
 
+    {/*<Message /> */} 
    {/*<Greet name="Bruce" heroName ="Batman">
      <p>This is chlidren props </p>
     </Greet>
